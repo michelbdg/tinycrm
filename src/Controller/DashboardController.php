f6be1214eb84cb1129b91bb7f3736644737ecac0
Jensone <hello@jensone.com>
2024-01-30 19:20:59 +0100

    🔧 corriger(DashboardController.php) : supprimer les commentaires inutiles et réorganiser les sections du menu ✨ fonctionnalité(DashboardController.php) : maximiser le contenu du tableau de bord pour une meilleure expérience utilisateur

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -19,44 +19,29 @@ class DashboardController extends AbstractDashboardController
     #[Route('/', name: 'admin')]
     public function index(): Response
     {
-        // return parent::index();
-
-        // Option 1. You can make your dashboard redirect to some common page of your backend
-        //
-        // $adminUrlGenerator = $this->container->get(AdminUrlGenerator::class);
-        // return $this->redirect($adminUrlGenerator->setController(OneOfYourCrudController::class)->generateUrl());
-
-        // Option 2. You can make your dashboard redirect to different pages depending on the user
-        //
-        // if ('jane' === $this->getUser()->getUsername()) {
-        //     return $this->redirect('...');
-        // }
-
-        // Option 3. You can render some custom template to display a proper dashboard with widgets, etc.
-        // (tip: it's easier if your template extends from @EasyAdmin/page/content.html.twig)
-        //
         return $this->render('pages/dashboard.html.twig');
     }
 
     public function configureDashboard(): Dashboard
     {
         return Dashboard::new()
-            ->setTitle('<img src="/images/logo.png" height="40">');
+            ->setTitle('<img src="/images/logo.png" height="40">')
+            ->renderContentMaximized();
     }
 
     public function configureMenuItems(): iterable
     {
         yield MenuItem::linkToDashboard('Accueil', 'fa fa-home');
-        yield MenuItem::section('Gestion des utilisateurs');
+        yield MenuItem::section('Gestion de l\'activité');
         yield MenuItem::linkToCrud('Nos clients', 'fa fa-users', Client::class);
         yield MenuItem::linkToCrud('Suivi', 'fa fa-calendar', Interaction::class);
         yield MenuItem::linkToCrud('Nos offres', 'fa fa-cubes', Offre::class);
         yield MenuItem::linkToCrud('Transaction', 'fa fa-euro', Transaction::class);
-        yield MenuItem::section('Gestion des articles');
+        yield MenuItem::section('Gestion de contenu');
         yield MenuItem::linkToCrud('Les articles', 'fa fa-newspaper', Article::class);
-        yield MenuItem::section('Gestion des paramètres');
+
+        yield MenuItem::section('Paramètres');
         yield MenuItem::linkToCrud('Comptes', 'fa fa-user', User::class);
         yield MenuItem::linkToLogout('Déconnexion', 'fa fa-sign-out');
-        // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
     }
 }
