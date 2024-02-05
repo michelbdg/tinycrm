344f571160d77fbe950c8c9cf90a9e1d73d6c8e8
Michel Bondonga <bondongamichel@gmail.com>
2024-02-05 12:47:05 +0100

    Ajouter(PaymentController.php) : ajouter un contrôleur de paiement pour gérer les paiements

---
+++ src/Controller/PaymentController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use App\Service\Stripe;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PaymentController extends AbstractController
+{
+    #[Route('/payment', name: 'app_payment')]
+    public function index(): Response
+    {
+        return $this->render('payment/index.html.twig', [
+            'controller_name' => 'PaymentController',
+        ]);
+    }
+}
