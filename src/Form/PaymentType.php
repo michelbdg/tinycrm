edb97b8781a2b3c6379485f71e80f3b5d30ea7b3
Michel Bondonga <bondongamichel@gmail.com>
2024-02-06 09:50:39 +0100

    Mise en place du formulaire

---
+++ src/Form/PaymentType.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Offre;
+use App\Entity\Client;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+class PaymentType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('Client', EntityType::class, [
+                'class' => Client::class,
+                'choice_label' => 'nomComplet',
+            ])
+            ->add('offre', EntityType::class, [
+                'class' => Offre::class,
+                'choice_label' => 'titre',
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => 'Générer le lien de paiement',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            // Configure your form options here
+        ]);
+    }
+}
